1ec775790cb550672822b81b9adc1082096dd935
JEES23 <76464398+JEES23@users.noreply.github.com>
2022-04-04 23:05:38 +0900

    최종

--- 03월 18일 - 브루트 포스/2858.py
+++
@@ -1,9 +0,0 @@
-R, B = map(int, input().split())        #R,B 입력받기 
-
-area = R+B                      #R과 B을 더한 값 = 넓이 
-
-for i in range(3,area//2):      #넓이를 어떤 수를 나눴을 때 나머지가 0인 수들을 구하기 
-    if area % i == 0:
-        if (i-2)*(area//i-2) == B:  #갈색 부분의 넓이를 구한 후 B와 같은지 확인 
-            print(area//i, i)
-            break
